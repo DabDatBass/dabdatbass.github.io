441b1376aa8c72f7b67ab823e5ddb3a041cf6039
DabDatBass <73787383+DabDatBass@users.noreply.github.com>
2021-08-22 18:37:15 -0500

    Add files via upload

---
+++ code/index.js
@@ -0,0 +1,24 @@
+class silvxrtools {
+  getInfo () {
+    return {
+      id: 'someBlocks',
+      name: 'Some Blocks',
+      blocks: [{
+        opcode: 'myReporter',
+        blockType: BlockType.REPORTER,
+        text: 'letter [LETTER_NUM] of [TEXT]',
+        arguments: {
+          LETTER_NUM: {
+            type: ArgumentType.STRING,
+            defaultValue: '1'
+          },
+          TEXT: {
+            type: ArgumentType.STRING,
+            defaultValue: 'text'
+          }
+        }
+      }]
+    };
+  }
+}
+Scratch.extensions.register(new silvxrtools());
