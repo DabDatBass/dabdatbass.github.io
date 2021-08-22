e5324f4233414b1760a4cbcd8584ff864ed199ec
DabDatBass <73787383+DabDatBass@users.noreply.github.com>
2021-08-22 18:57:36 -0500

    AAAAAAAAAAAAA

--- code/index.js
+++ code/index.js
@@ -1,11 +1,13 @@
+var ct_icon = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAwIiBoZWlnaHQ9IjYwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KIDxnPgogIDx0aXRsZT5MYXllciAxPC90aXRsZT4KICA8ZWxsaXBzZSBzdHJva2Utd2lkdGg9IjAiIHRyYW5zZm9ybT0icm90YXRlKDE3LjY1MDEsIDQwMCwgMzAwKSIgcnk9IjE5NiIgcng9IjE5NiIgaWQ9InN2Z18yIiBjeT0iMzAwIiBjeD0iNDAwIiBzdHJva2U9IiMwMDAiIGZpbGw9IiM4MzExZTAiLz4KIDwvZz4KCjwvc3ZnPg==";
+
 class silvxrtools {
   constructor(runtime, extensionId) {
 	this.runtime = runtime;
   }
   getInfo () {
     return {
-      id: 'someBlocks',
-      name: 'Some Blocks',
+      id: 'silvxrtools',
+      name: 'silvxrtools',
       blocks: [
       {
         opcode: 'myReporter',
